10a02dac9bac6b6e05b1fb68bbe8481876399106
844264935@qq.com <844264935@qq.com>
2019-06-26 17:40:37 +0800

    添加用户拦截器

--- src/main/java/com/zzw/fenda/FendaApplication.java
+++ src/main/java/com/zzw/fenda/FendaApplication.java
@@ -3,13 +3,20 @@ package com.zzw.fenda;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.zzw.fenda.dao")
-public class FendaApplication {
+public class FendaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FendaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
