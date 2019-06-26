5d48397ed8ffb01ffecf68adaa3abfc4bf37387e
844264935@qq.com <844264935@qq.com>
2019-06-26 11:08:18 +0800

    完成提问控制器开发

--- src/main/java/com/zzw/fenda/controller/AskController.java
+++ src/main/java/com/zzw/fenda/controller/AskController.java
@@ -1,7 +1,9 @@
 package com.zzw.fenda.controller;
 
 import com.zzw.fenda.dao.QueslistDao;
+import com.zzw.fenda.dto.AskListDTO;
 import com.zzw.fenda.dto.AskListFilter;
+import com.zzw.fenda.po.Queslist;
 import com.zzw.fenda.util.JsonR;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -23,7 +26,19 @@ public class AskController {
     public JsonR publish(Integer solverID,
                          String quesInfo,
                          HttpServletRequest request) {
-        return null;
+        try {
+            Integer giverID = (Integer) request.getAttribute("uid");
+            Queslist queslist = new Queslist();
+
+            queslist.setGiverID(giverID)
+                    .setSolverID(solverID)
+                    .setQuesInfo(quesInfo);
+
+            queslistDao.insertAndGetId(queslist);
+            return JsonR.createSuccess(queslist.getQuesID());
+        } catch (Exception e) {
+            return JsonR.createFail(e.getMessage());
+        }
     }
 
     @RequestMapping(value = "/reply", method = RequestMethod.POST)
@@ -31,7 +46,24 @@ public class AskController {
                        String answer,
                        String audioUrl,
                        HttpServletRequest request) {
-        return null;
+        try {
+            Integer solverID = (Integer) request.getAttribute("uid");
+            Queslist queslist;
+
+            if ((queslist = queslistDao.selectById(quesID)) == null) {
+                throw new Exception("问题不存在");
+            }
+            if (!queslist.getSolverID().equals(solverID)) {
+                throw new Exception("你不是该问题的回答者");
+            }
+            queslist.setAnswer(answer)
+                    .setAudioUrl(audioUrl);
+
+            queslistDao.updateReplyById(queslist);
+            return JsonR.createSuccess(queslist.getQuesID());
+        } catch (Exception e) {
+            return JsonR.createFail(e.getMessage());
+        }
     }
 
     @RequestMapping("/list")
@@ -39,12 +71,34 @@ public class AskController {
                       Integer order,
                       Integer page,
                       Integer limit) {
-        return null;
+        try {
+            page = page == null ? 1 : page;
+            limit = limit == null ? 10 : limit;
+            int offset = (page - 1) * limit;
+
+            List<AskListDTO> askListDTOList;
+            askListDTOList = queslistDao.listQuesByFilter(askListFilter, limit, offset);
+
+            return JsonR.createSuccess(askListDTOList);
+
+        } catch (Exception e) {
+            return JsonR.createFail(e.getMessage());
+        }
     }
 
     @RequestMapping("/get")
     public JsonR get(Integer quesID) {
-        return null;
+        try {
+            Queslist queslist;
+
+            if ((queslist = queslistDao.selectById(quesID)) == null) {
+                throw new Exception("问题不存在");
+            }
+            return JsonR.createSuccess(queslist);
+
+        } catch (Exception e) {
+            return JsonR.createFail(e.getMessage());
+        }
     }
 
 
