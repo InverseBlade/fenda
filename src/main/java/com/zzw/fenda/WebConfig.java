3cc9a25f3af8585210a3c22037ddb727641ace64
844264935@qq.com <844264935@qq.com>
2019-06-26 17:40:30 +0800

    添加用户拦截器

---
+++ src/main/java/com/zzw/fenda/WebConfig.java
@@ -0,0 +1,26 @@
+package com.zzw.fenda;
+
+import com.zzw.fenda.interceptor.UserInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Resource
+    UserInterceptor userInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index")
+                .excludePathPatterns("/index/**")
+                .excludePathPatterns("/test/**")
+                .excludePathPatterns("/upload/**");
+
+    }
+}
